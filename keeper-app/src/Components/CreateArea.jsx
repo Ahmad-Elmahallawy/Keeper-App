4e350ef894cfe5969e1e0d0fafbd4178c89cbf7b
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2023-02-21 17:19:37 -0500

    implemented delete functionality + done w project

--- keeper-app/src/Components/CreateArea.jsx
+++ keeper-app/src/Components/CreateArea.jsx
@@ -3,25 +3,21 @@ import React, { useState } from "react";
 const CreateArea = (props) => {
   const [note, setNote] = useState({
     title: "",
-    content: ""
+    content: "",
   });
 
-  
-
   function handleChange(event) {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
 
-    setNote(prevNote => {
-        return{
-            ...prevNote,
-            [name]: value
-        }
-    })
-
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
   }
 
   function submitNote(event) {
-
     props.onAdd(note);
     event.preventDefault();
   }
@@ -43,7 +39,7 @@ const CreateArea = (props) => {
           placeholder="Take a note..."
           onChange={handleChange}
         />
-        <button onClick = {submitNote}>Add</button>
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
